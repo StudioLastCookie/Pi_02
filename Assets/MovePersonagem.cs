585836376368fbf5953cf308576dd391d3d85d2c
StudioLastCookie <lastcookiestudio@gmail.com>
2014-12-04 12:22:04 -0200

    commit inicial

---
+++ Assets/MovePersonagem.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class MovePersonagem : MonoBehaviour {
+
+	List<IComando> historico;
+	
+	public float Angulo;
+	public 	float Velocidade;
+
+
+
+	IComando _Frente;
+	IComando _Lado;
+	public Transform Atual;
+
+	void Start () 
+	{
+		Atual.renderer.material.color = Color.blue;
+	}
+
+	void OnMouseDown ()
+	{
+	//	Debug.Log ("Clikou");
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+		float Vetical = Input.GetAxis ("Vertical");
+		float Horizontal = Input.GetAxis ("Horizontal");
+
+		if( Vetical != 0 )
+		
+		{
+			_Frente = new Mover (Atual,Direcao.Norte,Velocidade,Vetical,ref historico);
+			_Frente.Executar(false);
+			//Atual.Translate (Vector3.forward.normalized * Velocidade * Time.deltaTime);
+		}
+		if(Horizontal != 0 )
+		{
+			_Lado = new Mover (Atual, Direcao.Leste,Velocidade,Horizontal, ref historico);
+			_Lado.Executar (false);
+		}
+	//	print (historico.Count);
+	}
+}
